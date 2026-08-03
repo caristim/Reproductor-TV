b326a98a9d65b2cc9ccc6a30ff1f46bfd46026c3
caristim <carlosar@adinet.com.uy>
2026-08-03 10:05:16 -0300

    Add SESSION_API and DOMINIO to config.js

--- config.js
+++ config.js
@@ -11,6 +11,8 @@ const CONFIG = {
   SETUP_API: 'https://veratv-be.vera.com.uy/api/setup',
   GRID_API: 'https://cds-frontend.vera.com.uy/api-contenidos/listas/234',
   GRID_HEADERS: { 'x-service-id': '3', 'x-frontend-id': '1196', 'x-system-id': '1' },
+  SESSION_API: 'https://veratv-be.vera.com.uy/api/sesiones',
+  DOMINIO: 'lua',
 
   // --- Márgenes de renovación (renovar ANTES de que expire, no cuando ya expiró) ---
   SESSION_RENEW_MARGIN_MS: 10 * 60 * 1000,   // renovar sesión 10 min antes de que venza (~cada 6-8hs)
