107e9bea6ef3047e8d8d879a318b112d8f2fa844
caristim <carlosar@adinet.com.uy>
2026-08-06 17:47:29 -0300

    Update GRID_API endpoint in config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   LOGIN_API: '/api/login',
   SETUP_API: 'https://veratv-be.vera.com.uy/api/setup',
-  GRID_API: 'https://cds-frontend.vera.com.uy/api-contenidos/listas/234',
+  GRID_API: 'https://cds-frontend.vera.com.uy/api-contenidos/listas/68',
   GRID_HEADERS: { 'x-service-id': '3', 'x-frontend-id': '1196', 'x-system-id': '1' },
   SESSION_API: 'https://veratv-be.vera.com.uy/api/sesiones',
   DOMINIO: 'lua',
